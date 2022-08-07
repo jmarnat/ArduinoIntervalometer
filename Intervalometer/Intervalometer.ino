aa17f3932cf29c4569eca5ba5f0f3b55f41003d0
Josselin MARNAT <josselin.marnat@gmail.com>
2022-08-07 20:01:16 +0200

    trying with Thread.h but not working for now...

--- Intervalometer/Intervalometer.ino
+++ Intervalometer/Intervalometer.ino
@@ -1,3 +1,7 @@
+#include <ThreadController.h>
+#include <StaticThreadController.h>
+#include <Thread.h>
+
 int SER_Pin = 2;   //pin 14 on the 75HC595
 int RCLK_Pin = 3;  //pin 12 on the 75HC595
 int SRCLK_Pin = 4; //pin 11 on the 75HC595
@@ -15,46 +19,54 @@ const byte DIGITS[10] = {
   0b11110110
 };
 
+
+
+
 // for now nreg = 0 only (should be 0/1/2)
 // dig = 0 -> 9
 void setDigit(int dig, int nreg) {
-  Serial.print("digit: ");
-  Serial.println(dig);
-  
   byte dig_bits = DIGITS[dig];
-  Serial.println(dig_bits);
-
   digitalWrite(RCLK_Pin, LOW);
   for (int nbit = 0; nbit < 8; nbit++) {
     bool dig_bit = ! bitRead(dig_bits, nbit);
-    Serial.print(dig_bit);
-    digitalWrite(SRCLK_Pin, LOW);
-    digitalWrite(SER_Pin, dig_bit);
-    digitalWrite(SRCLK_Pin, HIGH);
     dig_bits >> 1;
   }
   digitalWrite(RCLK_Pin, HIGH);
-  Serial.println();
-  Serial.println();
 }
 
 
 
+int currentDigit = 2;
+// updates the display eveny x ms (should be run in a pseudo-thread
+// see https://github.com/ivanseidel/ArduinoThread
+void updateDisplay() {
+  Serial.print("updateDisplay with digit ");
+  Serial.println(currentDigit);
+  setDigit(currentDigit, 0);
+  currentDigit = (currentDigit + 1) % 10;
+  Serial.println("updateDisplay stop");
+}
+
+Thread displayThread = Thread();
+
 void setup() {
+  Serial.begin(9600);
+  
   pinMode(SER_Pin, OUTPUT);
   pinMode(RCLK_Pin, OUTPUT);
   pinMode(SRCLK_Pin, OUTPUT);
 
-  Serial.begin(9600);
+  displayThread.enabled = true;
+  displayThread.setInterval(500);
+  displayThread.onRun(updateDisplay);
 
-  setDigit(2, 0);
+
+  Serial.println("Initial run");
+  displayThread.run();
 }
 
 
 void loop() {
-  for (int d = 0; d <= 9; d++) {
-    setDigit(d, 0);
-    delay(500);
-  }
-  delay(500);
+  // Serial.println("standby");
+  // nothing for now
 }
